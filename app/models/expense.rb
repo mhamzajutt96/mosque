fe64c6120ff895b032abce158610696fba49d320
Muhammad Hamza <mhamzajutt96@gmail.com>
2021-07-14 21:04:27 +0500

    Add the created_at column to the expenses table.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,9 +2,11 @@
 
 # :Expense Model:
 class Expense < ApplicationRecord
+  # Associations
   belongs_to :masjid
   has_one :employee_expense, dependent: :destroy
   has_one :employee, through: :employee_expense
   has_one :bill, dependent: :destroy
+  # Validations
   validates_presence_of :amount, :reason
 end
